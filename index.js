683698fd9c6304c9f7c7b95937c9ff00dbae8318
mario1702 <79115927+mario1702@users.noreply.github.com>
2025-05-30 06:49:27 +0200

    index.js

--- index.js
+++ index.js
@@ -1,44 +1,45 @@
-
 const net = require("net");
-const WebSocket = require("ws");
 const http = require("http");
+const WebSocket = require("ws");
 
 const poolHost = "pool.supportxmr.com";
 const poolPort = 3333;
 
 const server = http.createServer();
+
 const wss = new WebSocket.Server({ server });
 
-wss.on("connection", function connection(ws) {
-    let poolSocket = new net.Socket();
+wss.on("connection", ws => {
+  const poolSocket = new net.Socket();
 
-    poolSocket.connect(poolPort, poolHost, () => {
-        console.log("Connected to SupportXMR pool");
-    });
+  poolSocket.connect(poolPort, poolHost, () => {
+    console.log("Connected to SupportXMR pool");
+  });
 
-    poolSocket.on("data", (data) => {
-        ws.send(data);
-    });
+  poolSocket.on("data", data => {
+    ws.send(data);
+  });
 
-    ws.on("message", (message) => {
-        poolSocket.write(message);
-    });
+  ws.on("message", message => {
+    poolSocket.write(message);
+  });
 
-    ws.on("close", () => {
-        poolSocket.end();
-    });
+  ws.on("close", () => {
+    poolSocket.end();
+  });
 
-    poolSocket.on("close", () => {
-        ws.close();
-    });
+  poolSocket.on("close", () => {
+    ws.close();
+  });
 
-    poolSocket.on("error", (err) => {
-        console.error("Pool socket error:", err);
-        ws.close();
-    });
+  poolSocket.on("error", err => {
+    console.error("Pool socket error:", err);
+    ws.close();
+  });
 });
 
 const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => {
-    console.log(`Proxy server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
